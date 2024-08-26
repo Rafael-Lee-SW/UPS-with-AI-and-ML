6e1f83683186407fd9a0512d51d424d64970bf29
Soowan <ale783@naver.com>
2024-08-26 09:46:34 +0900

    callback : 이전 프로젝트 불러오기 backend

---
+++ wms_backend/src/main/java/com/a508/wms/auth/service/AuthService.java
@@ -0,0 +1,24 @@
+package com.a508.wms.auth.service;
+
+import com.a508.wms.auth.dto.request.auth.CheckCertificationRequestDto;
+import com.a508.wms.auth.dto.request.auth.EmailCertificationRequestDto;
+import com.a508.wms.auth.dto.request.auth.SignInRequestDto;
+import com.a508.wms.auth.dto.request.auth.SignUpRequestDto;
+import com.a508.wms.auth.dto.response.auth.CheckCertificationResponseDto;
+import com.a508.wms.auth.dto.response.auth.EmailCertificationResponseDto;
+import com.a508.wms.auth.dto.response.auth.IdCheckResponseDto;
+import com.a508.wms.auth.dto.response.auth.SignInResponseDto;
+import com.a508.wms.auth.dto.response.auth.SignUpResponseDto;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+
+public interface AuthService {
+
+    ResponseEntity<? super IdCheckResponseDto> idCheck(@Valid EmailCertificationRequestDto dto);
+    ResponseEntity<? super EmailCertificationResponseDto> emailCertification(
+        EmailCertificationRequestDto dto);
+   ResponseEntity<? super CheckCertificationResponseDto> checkCertification(CheckCertificationRequestDto dto);
+   ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto);
+   ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto);
+
+}
---
+++ wms_backend/src/main/java/com/a508/wms/product/dto/ExportResponseDto.java
@@ -0,0 +1,30 @@
+package com.a508.wms.product.dto;
+
+import com.a508.wms.util.constant.ProductStorageTypeEnum;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class ExportResponseDto {
+
+    private String trackingNumber;
+    private Long barcode;
+    private String locationName;
+    private String productName;
+    private int quantity;
+    private int floorLevel;
+    private LocalDateTime date;
+    private LocalDateTime expirationDate;
+    private ProductStorageTypeEnum productStorageType;
+
+    private String warehouseName;
+    private Long warehouseId;
+}
