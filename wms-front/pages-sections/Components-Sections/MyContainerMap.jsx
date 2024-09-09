7d2093c52ffad9aaa885884763bba8aa7ed88650
Soowan <ale783@naver.com>
2024-09-09 13:46:54 +0900

    style : separate the style of MyContainerMap.jsx

--- wms-front/pages-sections/Components-Sections/MyContainerMap.jsx
+++ wms-front/pages-sections/Components-Sections/MyContainerMap.jsx
@@ -23,7 +23,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 // core components
 import Button from "/components/CustomButtons/Button.js";
 // CSS스타일
-import styles from "/styles/jss/nextjs-material-kit/pages/componentsSections/MyContainerStyle.jsx";
+import styles from "/styles/jss/nextjs-material-kit/pages/componentsSections/MyContainerMapStyle";
 //Material UI 창고 생성 테스트를 위한
 import { Typography, Slider, Box, Modal, Fade, TextField } from "@mui/material";
 
@@ -33,93 +33,7 @@ const GRID_SIZE_SUB_50 = 50;
 const GRID_SIZE_SUB_10 = 10;
 const CANVAS_SIZE = 1000;
 
-const useStyles = makeStyles((theme) => ({
-  ...styles,
-  canvasContainer: {
-    position: "relative",
-    width: "100%",
-    height: "100vh",
-    overflow: "hidden",
-  },
-  leftSidebar: {
-    position: "absolute",
-    top: "10vh",
-    left: 0,
-    width: "200px",
-    height: "80vh",
-    backgroundColor: "rgba(247, 247, 247, 0.9)",
-    boxShadow: "2px 0 5px rgba(0, 0, 0, 0.1)",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    zIndex: 1100,
-    padding: "10px 10px 10px 15px",
-    overflowY: "auto",
-  },
-  rightSidebar: {
-    position: "absolute",
-    top: "10vh",
-    right: 15,
-    width: "220px",
-    height: "80vh",
-    backgroundColor: "rgba(247, 247, 247, 0.9)",
-    boxShadow: "-2px 0 5px rgba(0, 0, 0, 0.1)",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    zIndex: 1100,
-    padding: "10px 0",
-    overflowY: "auto",
-  },
-  buttonStyle: {
-    backgroundColor: "transparent",
-    width: "50px",
-    color: "#7D4A1A",
-    marginLeft: "10px",
-    marginTop: "30px",
-    height: "30px",
-    border: "1px solid #7D4A1A",
-    borderRadius: "4px",
-    "&:hover": {
-      transform: "scale(1.05)",
-      backgroundColor: "#7D4A1A",
-      color: "white",
-    },
-  },
-  generateButton: {
-    backgroundColor: "#7D4A1A",
-    fontSize: "15px",
-    "&:hover": {
-      transform: "scale(1.05)",
-      backgroundColor: "transparent",
-      border: "1px solid #7D4A1A",
-      color: "#7D4A1A",
-    },
-  },
-  outOfCanvas: {
-    position: "relative",
-    width: "100%",
-    height: "100vh",
-  },
-  inOfCanvas: {
-    width: "100%",
-    height: "100vh",
-    cursor: "default",
-  },
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    maxWidth: "500px",
-    width: "90%",
-  },
-}));
+const useStyles = makeStyles(styles);
 
 /**
  * 창고 관리 Component
