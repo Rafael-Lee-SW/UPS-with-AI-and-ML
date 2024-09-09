4a871fc12c2b53aa84f98a2a7f68b485d1ca3aa3
Soowan <ale783@naver.com>
2024-09-09 17:31:19 +0900

    feat : now available pwa

--- wms-front/next.config.js
+++ wms-front/next.config.js
@@ -1,6 +1,15 @@
+// Import the PWA configuration
+const withPWA = require('next-pwa')({
+  dest: 'public',       // PWA와 관련한 파일들을 생성할 디렉토리 지정
+  register: true,       // Automatically register the service worker
+  skipWaiting: true,    // Forces the app to use the new service worker once it's updated
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-};
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  // Add any other Next.js configuration here
+});
 
 module.exports = nextConfig;
