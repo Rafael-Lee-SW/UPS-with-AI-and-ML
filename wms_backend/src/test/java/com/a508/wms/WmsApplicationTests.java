a8db62373e8776cc019cb918aec475729aebe7bc
김준혁 <plrare.dev@gmail.com>
2024-09-10 12:45:02 +0900

    test: WmsApplicationTests 주석 처리

--- wms_backend/src/test/java/com/a508/wms/WmsApplicationTests.java
+++ wms_backend/src/test/java/com/a508/wms/WmsApplicationTests.java
@@ -3,7 +3,8 @@ package com.a508.wms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+// @SpringBootTest
 class WmsApplicationTests {
 
 //	@Test
