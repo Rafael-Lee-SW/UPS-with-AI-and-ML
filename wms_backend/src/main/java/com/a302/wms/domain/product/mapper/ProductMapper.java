291dcea96761499d865837646456de24ba7a0bb6
김서영 <popolley@gmail.com>
2024-10-02 09:37:16 +0900

    ♻️ Refactor: ProductMapper 수정

--- wms_backend/src/main/java/com/a302/wms/domain/product/mapper/ProductMapper.java
+++ wms_backend/src/main/java/com/a302/wms/domain/product/mapper/ProductMapper.java
@@ -3,7 +3,6 @@ package com.a302.wms.domain.product.mapper;
 import com.a302.wms.domain.floor.entity.Floor;
 import com.a302.wms.domain.product.dto.ProductImportRequest;
 import com.a302.wms.domain.product.dto.ProductResponse;
-import com.a302.wms.domain.product.dto.ProductWithUserResponse;
 import com.a302.wms.domain.product.entity.Product;
 import com.a302.wms.domain.store.entity.Store;
 import org.springframework.stereotype.Component;
@@ -26,22 +25,25 @@ public class ProductMapper {
                 .quantity(product.getQuantity())
                 .floorLevel(product.getFloor().getFloorLevel())
                 .locationName(product.getFloor().getLocation().getName())
+                .originalPrice(product.getOriginalPrice())
+                .sellingPrice(product.getSellingPrice())
+                .storeId(product.getStore().getId())
                 .build();
     }
 
     public static Product fromProductImportRequest(ProductImportRequest productImportRequest,
                                                    Floor floor,
                                                    Store store) {
-    return Product.builder()
-        .barcode(productImportRequest.barcode())
-        .floor(floor)
-        .originalPrice(productImportRequest.originalPrice())
-        .productName(productImportRequest.productName())
-        .quantity(productImportRequest.quantity())
-        .sellingPrice(productImportRequest.sellingPrice())
-        .sku(productImportRequest.sku())
-        .store(store)
-        .build();
+        return Product.builder()
+                .barcode(productImportRequest.barcode())
+                .floor(floor)
+                .originalPrice(productImportRequest.originalPrice())
+                .productName(productImportRequest.productName())
+                .quantity(productImportRequest.quantity())
+                .sellingPrice(productImportRequest.sellingPrice())
+                .sku(productImportRequest.sku())
+                .store(store)
+                .build();
     }
 
 }
