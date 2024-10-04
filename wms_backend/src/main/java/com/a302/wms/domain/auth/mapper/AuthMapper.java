d56da7cca7c44f0045fc91e84d25e145c83fc651
김서영 <popolley@gmail.com>
2024-10-04 11:02:04 +0900

    ♻️ Refactor: kiosk 로그인인 경우 DeviceSignInResponse에 상품 목록까지 함께 반환하도록 추가

--- wms_backend/src/main/java/com/a302/wms/domain/auth/mapper/AuthMapper.java
+++ wms_backend/src/main/java/com/a302/wms/domain/auth/mapper/AuthMapper.java
@@ -3,8 +3,11 @@ package com.a302.wms.domain.auth.mapper;
 import com.a302.wms.domain.auth.dto.response.DeviceSignInResponse;
 import com.a302.wms.domain.auth.dto.response.UserSignInResponse;
 import com.a302.wms.domain.device.dto.DeviceResponse;
+import com.a302.wms.domain.product.dto.ProductResponse;
 import com.a302.wms.domain.user.dto.UserResponse;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class AuthMapper {
@@ -16,10 +19,11 @@ public class AuthMapper {
                 .build();
     }
 
-    public static DeviceSignInResponse fromDeviceToken(String deviceToken, DeviceResponse deviceResponse) {
+    public static DeviceSignInResponse fromDeviceToken(String deviceToken, DeviceResponse deviceResponse, List<ProductResponse> productResponseList) {
         return DeviceSignInResponse.builder()
                 .accessToken(deviceToken)
                 .deviceResponse(deviceResponse)
+                .productResponseList(productResponseList)
                 .build();
     }
 }
