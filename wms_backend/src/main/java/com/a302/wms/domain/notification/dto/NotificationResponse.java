eaf957e23bf9bfd05efe85230c202c62130c1045
김준혁 <plrare.dev@gmail.com>
2024-10-06 19:14:29 +0900

    feature: notification 테이블 추가 및 영상 업로드시 저장하도록 함, 영상 업로드 api endpoint 변경

---
+++ wms_backend/src/main/java/com/a302/wms/domain/notification/dto/NotificationResponse.java
@@ -0,0 +1,17 @@
+package com.a302.wms.domain.notification.dto;
+
+import com.a302.wms.global.constant.NotificationTypeEnum;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+public record NotificationResponse(
+        Long id,
+        Long userId,
+        Long storeId,
+        Boolean isRead,
+        Boolean isImportant,
+        String message,
+        NotificationTypeEnum notificationTypeEnum
+) {
+}
