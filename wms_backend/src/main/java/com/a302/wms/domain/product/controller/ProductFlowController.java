b625caf27361b6193b50278440632749b6dbf7a4
김준혁 <plrare.dev@gmail.com>
2024-10-07 15:07:18 +0900

    ✨ Feature: 매장 전체 알림 조회, 매장에서 타입 구분하여 알림 조회하는 기능 추가 ProductFlow 관련 Enum 및 Notification 관련 Enum 수정 기본 메시지 Constant 추가

--- wms_backend/src/main/java/com/a302/wms/domain/product/controller/ProductFlowController.java
+++ wms_backend/src/main/java/com/a302/wms/domain/product/controller/ProductFlowController.java
@@ -2,10 +2,14 @@ package com.a302.wms.domain.product.controller;
 
 import com.a302.wms.domain.product.dto.ProductFlowResponse;
 import com.a302.wms.domain.product.service.ProductFlowServiceImpl;
+import com.a302.wms.global.constant.ResponseEnum;
+import com.a302.wms.global.response.BaseExceptionResponse;
 import com.a302.wms.global.response.BaseSuccessResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,9 +20,16 @@ public class ProductFlowController {
   private final ProductFlowServiceImpl productFlowServiceImpl;
 
   @GetMapping
-  public BaseSuccessResponse<List<ProductFlowResponse>> getAllProductFlows(
-      @RequestParam(name = "userId") Long userId) {
-
-    return new BaseSuccessResponse<>(productFlowServiceImpl.findAllByUserId(userId));
+  public ResponseEntity<?> getAllProductFlows(
+      @RequestParam(name = "userId", required = false) Long userId,
+      @RequestParam(name = "storeId", required = false) Long storeId) {
+    if (storeId != null) {
+      return ResponseEntity.ok(new BaseSuccessResponse<>(productFlowServiceImpl.findAllByStoreId(storeId)));
+    }
+    else if(userId != null) {
+    return ResponseEntity.ok(new BaseSuccessResponse<>(productFlowServiceImpl.findAllByUserId(userId)));
+    } else return ResponseEntity.badRequest().body(new BaseExceptionResponse(false,ResponseEnum.SERVER_ERROR.getStatusCode(),
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            ResponseEnum.SERVER_ERROR.getMessage()));
   }
 }
