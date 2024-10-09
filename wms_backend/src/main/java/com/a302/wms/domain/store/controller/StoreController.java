f68bda399e2efa4a67d7cc149a0b70b5d3f639d0
김준혁 <plrare.dev@gmail.com>
2024-10-09 09:46:40 +0900

    🐛 fix : StoreController의 saveStructure에 있는 userId가 다시 AuthenticationPrincipal에서 가져올 수 있도록 변경

--- wms_backend/src/main/java/com/a302/wms/domain/store/controller/StoreController.java
+++ wms_backend/src/main/java/com/a302/wms/domain/store/controller/StoreController.java
@@ -91,7 +91,7 @@ public class StoreController {
 
     @PostMapping("/{storeId}/structures")
     public BaseSuccessResponse<Void> saveStructure(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal Long userId,
             @PathVariable Long storeId,
             @RequestBody StructureCreateRequest structureCreateRequest
     ) {
