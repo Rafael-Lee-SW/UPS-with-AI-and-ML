b455a861596c1c9f30dd529caf57a11daaa8519d
김준혁 <plrare.dev@gmail.com>
2024-10-10 13:22:38 +0900

    ✨ Feature: storeId로 camera 테이블 조회시 해당하는 storeId의 정보만 return하는 기능 추가

--- wms_backend/src/main/java/com/a302/wms/domain/s3/service/S3ServiceImpl.java
+++ wms_backend/src/main/java/com/a302/wms/domain/s3/service/S3ServiceImpl.java
@@ -1,38 +1,34 @@
 package com.a302.wms.domain.s3.service;
 
 import com.a302.wms.domain.s3.dto.S3DownloadLinkResponse;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-
-import java.time.Duration;
 
 @Service
 public class S3ServiceImpl {
 
-    private final S3Presigner s3Presigner;
-    private final String bucketName;
+  private final S3Presigner s3Presigner;
+  private final String bucketName;
 
-    public S3ServiceImpl(S3Presigner s3Presigner, @Value("${aws.s3.bucket}") String bucketName) {
-        this.s3Presigner = s3Presigner;
-        this.bucketName = bucketName;
-    }
+  public S3ServiceImpl(S3Presigner s3Presigner, @Value("${aws.s3.bucket}") String bucketName) {
+    this.s3Presigner = s3Presigner;
+    this.bucketName = bucketName;
+  }
 
-    public S3DownloadLinkResponse generatePresignedUrl(String objectKey) {
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(bucketName)
-                .key(objectKey)
-                .build();
+  public S3DownloadLinkResponse generatePresignedUrl(String objectKey) {
+    GetObjectRequest getObjectRequest =
+        GetObjectRequest.builder().bucket(bucketName).key(objectKey).build();
 
-        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofDays(7)) // URL 유효 기간 설정
-                .getObjectRequest(getObjectRequest)
-                .build();
-
-        return S3DownloadLinkResponse.builder()
-                .downloadLink(s3Presigner.presignGetObject(presignRequest).url().toString())
-                .build();
-    }
+    GetObjectPresignRequest presignRequest =
+        GetObjectPresignRequest.builder()
+            .signatureDuration(Duration.ofDays(7)) // URL 유효 기간 설정
+            .getObjectRequest(getObjectRequest)
+            .build();
+    String url = s3Presigner.presignGetObject(presignRequest).url().toString();
+    return S3DownloadLinkResponse.builder().downloadLink(url).build();
+  }
 }
